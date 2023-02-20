abdefb5fede60458eb2476a4adb1bb21456d98d4
NatanelG <natigirma02@gmail.com>
2023-02-20 12:12:45 +0300

    props added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Header from './components/Header';
 import About from './components/About';
 import Interests from './components/Interests';
 import Footer from './components/Footer';
-
+import data from './data/data.json';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header data={data}/>
       <div className='main'>
         <About />
         <Interests />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import Profile from '../images/profile-1.jpg';
 
-export default function Header() {
+export default function Header(props) {
 
-    const name = "Guest";
     const emailURL = "mailto:nnheo@example.com";
     const linkedInURL = "https://www.linkedin.com/in/joshua-m";
     return (
         <header>
-            <img className='header-image' src={Profile} alt={name}/>
+            <img className='header-image' src={Profile} alt={props.data.first_name}/>
             <div className='header-content'>
-                <h3 className='header-name'>New User</h3>
-                <p className='header-carrier'>Desinger</p>
-                <p className='header-desc'>First react app</p>
+                <h3 className='header-name'>{`${props.data.first_name} ${props.data.last_name}`}</h3>
+                <p className='header-carrier'>{props.data.carrier}</p>
+                <p className='header-desc'>{props.data.description}</p>
                 <div className='header-a'>
                     <a className='header-email' href={emailURL}>Email</a>
                     <a className='header-linkedIn' href={linkedInURL}>LinkedIn</a>
